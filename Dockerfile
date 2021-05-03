74a8d450dcbfd4001b0cf9e5f7686bf6bcf13eaf
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-05-03 17:36:02 +0100

    chore: minor fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM node:12-alpine
-WORKDIR /shape-area-api
 COPY . .
 RUN npm install --production
 CMD ["node", "index.js"]
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 const Joi = require("joi");
-const crypto = require("crypto");
 const { User } = require("../models");
 const jwt = require("jsonwebtoken");
 
